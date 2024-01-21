41a7e03d708ab06fe5495406cca13640aed9933b
btstbr <95320986+btstbr@users.noreply.github.com>
2024-01-20 23:18:22 -0300

    commit

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+
+    // Update the first paragraph in the footer with the copyright year
+    const copyrightParagraph = document.querySelector('.copyright_text');
+    copyrightParagraph.textContent = `© ${currentYear} Rafael Batista, Brazil`;
+
+    // Update the second paragraph in the footer with the last modified date
+    const lastModifiedParagraph = document.querySelector('.last_modified');
+    const lastModifiedDate = document.lastModified;
+    lastModifiedParagraph.textContent = `Last Modified: ${lastModifiedDate}`;
+});
